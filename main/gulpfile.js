d2e8a91d276c63ea155c445d7d6e023ffceacc27
vipulism <vipul0809@gmail.com>
2017-11-01 23:48:28 +0530

    postcss add

--- main/gulpfile.js
+++ main/gulpfile.js
@@ -1,24 +1,20 @@
 'use strict';
 
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+var postcss    = require('gulp-postcss');
 var sourcemaps = require('gulp-sourcemaps');
 
 
-gulp.task('sass', function () {
-    return gulp.src('./sass/**/*.scss')
-     .pipe(sourcemaps.init())
-     .pipe(sass().on('error', sass.logError))
-     .pipe(sourcemaps.write())
-     .pipe(gulp.dest('./css'));
-   });
 
 
-   gulp.task('sass:watch', function () {
-    gulp.watch('./sass/**/*.scss', ['sass']);
-  });
-
+gulp.task('css', function () {
   
+  return gulp.src('src/postcss/*.css')
+      .pipe( sourcemaps.init() )
+     .pipe( postcss(/*[ require('precss'), require('autoprefixer') ]*/) )
+      .pipe( sourcemaps.write('.') )
+      .pipe( gulp.dest('css/') );
+});
 
 gulp.task('default', function() {
     // place code for your default task here
