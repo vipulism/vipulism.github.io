da386d4005bf7e91a0270b2e5a7a8bdfea06e5a1
Vipul Sharma <vipul.sharma@prodigi.local>
2017-11-02 18:46:58 +0530

    setup postcss plugins with scss

--- main/gulpfile.js
+++ main/gulpfile.js
@@ -1,19 +1,27 @@
 'use strict';
 
 var gulp = require('gulp');
-var postcss    = require('gulp-postcss');
+var postcss = require('gulp-postcss');
+var sass = require('gulp-scss');
+var precss = require('precss');
+var syntax = require('postcss-scss');
+var autoprefixer  = require('autoprefixer');
+var short = require('postcss-short');
 var sourcemaps = require('gulp-sourcemaps');
 
-
-
+var processors = [
+    short(),
+    autoprefixer(),
+    precss(),
+];
 
 gulp.task('css', function () {
   
   return gulp.src('src/postcss/*.css')
-      .pipe( sourcemaps.init() )
-     .pipe( postcss(/*[ require('precss'), require('autoprefixer') ]*/) )
-      .pipe( sourcemaps.write('.') )
-      .pipe( gulp.dest('css/') );
+     .pipe( sourcemaps.init())
+     .pipe( postcss(processors))
+     .pipe( sourcemaps.write('.'))
+     .pipe( gulp.dest('css/'));
 });
 
 gulp.task('default', function() {
