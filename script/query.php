697e7d77a8ff6320b8e313fe7c6f34644637bc2c
vipul <vipul0809@gmail.com>
2026-01-18 19:08:36 +0530

    fixes for form ui and action

--- script/query.php
+++ script/query.php
@@ -1,23 +1,17 @@
 <?php
 
-$contact = $_REQUEST['contact'];
-$phone = $_REQUEST['phone'];
+$name = $_REQUEST['name'];
 $email = $_REQUEST['email'];
-$requirement=$_REQUEST['comment'];
+$message=$_REQUEST['message'];
 $ip=$_SERVER['REMOTE_ADDR'];
 $url=$_SERVER['HTTP_REFERER'];
  	
 $sub="Enquiry Mail from : ".$_POST['email'];
 	 $from=$_POST['email'];
 	 $msg ="From Page  :$url\n\n";
-	 $msg.="Contact Person:$contact\n";
-	 $msg.="Product Category:$product\n";
-	 $msg.="Company Name:$company\n";
-	 $msg.="Product Code:$code\n";
-	 $msg.="Contact No.:$phone\n";
-	 $msg.="Colour:$colour\n";
+	 $msg.="Name:$name\n";
 	 $msg.="Email:$email\n";
-	 $msg.="Quantity:$quantity\n";
+	 $msg.="Message:$message\n";
 	 $msg.="Ref Url:$url\n\n";
 	 $msg.="IP   :$ip\n";
       
@@ -25,8 +19,7 @@ $sub="Enquiry Mail from : ".$_POST['email'];
    $headers = "From: ${email}\nReply-To: ${email}";
 		
        
-	    mail("vipul0809@gmail.in ", "Enquiry Mail from :vipulism.github.io", "$msg", "From:$email");
-		//mail("ankitjain04@gmail.com", "Enquiry Mail from :ushawelding.com", "$msg", "From:$email");
+	mail("vipul0809@gmail.in ", "Enquiry Mail from :vipulism.github.io", "$msg", "From:$email");
          
 		
     echo "<script language=\"JavaScript\">\n";
